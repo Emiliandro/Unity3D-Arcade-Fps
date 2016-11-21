037c214e2d22823897f0e1f1a5f961c0c960e1a5
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2016-11-21 01:26:26 -0400

    Billboard -  Field of View

---
+++ League of Evil Mages/Assets/Scripts/Billboard.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour {
+
+    private Transform tf;
+    private Camera cameraMain;
+
+    private void Awake(){
+        tf = gameObject.transform;
+        cameraMain = Camera.main;
+    }
+
+
+    
+	// Update is called once per frame
+	void LateUpdate() {
+        Vector3 cameraPos = cameraMain.transform.position;
+        cameraPos.y = tf.position.y;
+        tf.LookAt(cameraPos);
+	
+	}
+}
