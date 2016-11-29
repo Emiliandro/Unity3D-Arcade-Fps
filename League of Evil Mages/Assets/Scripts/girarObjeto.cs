fde6582e0fe5fae97468891048389337f6c43943
RickoBraz <marco.henrique.s.s@gmail.com>
2016-11-29 14:13:49 -0400

    Colisão Moeda-Player

--- League of Evil Mages/Assets/Scripts/girarObjeto.cs
+++ League of Evil Mages/Assets/Scripts/girarObjeto.cs
@@ -4,9 +4,13 @@ using System.Collections;
 public class girarObjeto : MonoBehaviour
     {
 
-    void FixedUpdate()
-    {
+    void FixedUpdate(){
         transform.Rotate(new Vector3(0f, 60f, 0f) * Time.deltaTime);
 
+    }
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            Destroy(this.gameObject);
+        }
     }
 }
