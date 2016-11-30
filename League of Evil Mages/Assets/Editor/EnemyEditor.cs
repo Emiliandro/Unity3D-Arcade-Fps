6d7c04b2adbdedc4d5a3dcb30c62a5f85a90cecc
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2016-11-30 17:39:39 -0400

    Editor de Inimigos

---
+++ League of Evil Mages/Assets/Editor/EnemyEditor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class EnemyEditor : ScriptableWizard {
+
+    public string enemyName;
+    public int enemyLifePoint;
+    public int enemyDamage;
+    public bool isMove;
+    public float enemySpeed;
+
+    public Sprite enemySprite;
+
+
+    [MenuItem("My Tools/Create Enemy.")]
+    static void CreateWizard(){
+        ScriptableWizard.DisplayWizard<EnemyEditor>("Create Enemy", "Create New");
+    }
+
+    void OnWizardCreate(){
+        GameObject enemyGO = new GameObject();
+
+        if (isMove){
+            EnemyMove enemyMove = enemyGO.AddComponent<EnemyMove>();
+            enemyMove.speed = enemySpeed;
+        }
+
+        enemyGO.name = enemyName;
+        SpriteRenderer spriteRender = enemyGO.AddComponent<SpriteRenderer>();
+        spriteRender.sprite = enemySprite;
+        Animator anime = enemyGO.AddComponent<Animator>();
+
+        BoxCollider box = enemyGO.AddComponent<BoxCollider>();
+        Vector3 boxSize = spriteRender.bounds.size;
+        box.size = boxSize;
+        float x = Random.Range(210, 230);
+        float z = Random.Range(110, 135);
+
+        Debug.Log("Sprite render size: " + spriteRender.bounds.size);
+
+        enemyGO.transform.position = new Vector3(x, 2.47f, z);
+
+    }
+
+
+
+}
